e50f8957fec60bd9e1d3bb9806b2b2ce0f4d0f89
Koc Grebeniuk <kgrebeniuk@gmail.com>
2018-12-25 19:25:26 +0200

    line termination fix attempt

--- src/main/java/com/tgbot/omb/OurBot.java
+++ src/main/java/com/tgbot/omb/OurBot.java
@@ -1,1 +1,70 @@
-package com.tgbot.omb;import Speak.SpeakBot;import org.telegram.telegrambots.ApiContextInitializer;import org.telegram.telegrambots.bots.TelegramLongPollingBot;import org.telegram.telegrambots.meta.api.methods.send.SendMessage;import org.telegram.telegrambots.meta.api.objects.Message;import org.telegram.telegrambots.meta.api.objects.Update;import org.telegram.telegrambots.meta.exceptions.TelegramApiException;public class OurBot extends TelegramLongPollingBot {   // @Override    public void sendMsg(Message message, String text) {        SendMessage sendMessage = new SendMessage();        sendMessage.enableMarkdown(true);        sendMessage.setChatId(message.getChatId().toString());        sendMessage.setReplyToMessageId(message.getMessageId());        sendMessage.setText(text);        try {            execute(sendMessage);        } catch (TelegramApiException e) {            e.printStackTrace();        }    }    @Override    public void onUpdateReceived(Update update) {        // проверка есть ли в обновлении сообщение и текст сообщения        // очередь ожидающих запросов//        if (update.hasMessage() && update.getMessage().hasText()) {//            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields//                    .setChatId(update.getMessage().getChatId())//                    .setText(update.getMessage().getText());//            try {//                execute(message); // Call method to send the message//            } catch (TelegramApiException e) {//                e.printStackTrace();//            }//        }        Message message = update.getMessage();        if (message != null && message.hasText()) {            String response = SpeakBot.processIncomingMessage(message);            sendMsg(message, response);        }}    @Override    public String getBotUsername() {        // variable heroku Config Vars        //return System.getenv("username");        return "opravday_macbook_bot";    }    @Override    public String getBotToken() {        // variable heroku Config Vars        //return System.getenv("token");        //return null; //add our key        return "709217752:AAEvjRxFKJjejPE0q-3lOG_1k1_AR4rpaWo";    }}
+package com.tgbot.omb;
+
+
+import Speak.SpeakBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class OurBot extends TelegramLongPollingBot {
+
+
+   // @Override
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // проверка есть ли в обновлении сообщение и текст сообщения
+        // очередь ожидающих запросов
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+//                    .setChatId(update.getMessage().getChatId())
+//                    .setText(update.getMessage().getText());
+//            try {
+//                execute(message); // Call method to send the message
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            String response = SpeakBot.processIncomingMessage(message);
+            sendMsg(message, response);
+        }}
+
+
+
+    @Override
+    public String getBotUsername() {
+        // variable heroku Config Vars
+        //return System.getenv("username");
+        return "opravday_macbook_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // variable heroku Config Vars
+        //return System.getenv("token");
+        //return null; //add our key
+        return "709217752:AAEvjRxFKJjejPE0q-3lOG_1k1_AR4rpaWo";
+
+    }
+
+}
+
